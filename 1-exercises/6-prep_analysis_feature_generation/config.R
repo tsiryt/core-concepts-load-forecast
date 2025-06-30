33676b56316e7e0dbc80a2d93f193a27d039112a
Tsiry Rasendrasoa <rasendrasoatsiry@gmail.com>
2025-06-30 23:17:27 +0200

    preparation de l'exercice

---
+++ 1-exercises/6-prep_analysis_feature_generation/config.R
@@ -0,0 +1,1 @@
+filter_household <- c("household_type" = "indus", "id_household" = 0, "energy_type" = NA, "id_ener_source" = NA)
---
+++ 1-exercises/6-prep_analysis_feature_generation/main.R
@@ -0,0 +1,14 @@
+here::i_am("1-exercises/6-prep_analysis_feature_generation/main.R")
+
+source(here::here("libraries.R"))
+source(here::here("config.R"))
+
+source(here::here("1-exercises/6-prep_analysis_feature_generation/config.R"))
+
+# Take a demand time series. 
+log_info("Parametres : ")
+## Plot the autocorr & partial autocorr
+## At what lags is the autocorr strongest ?
+## Plot a scatter plot of the demand against lags of the demand series.
+## Are relationships linear ? If so, calculate adjusted coeffs of determination.
+## Which lags give the biggest values ?
