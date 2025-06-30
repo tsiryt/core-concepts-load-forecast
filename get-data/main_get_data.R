1a4f80fdb95e27ba7c14811f2dc79742749c6ff5
Tsiry Rasendrasoa <rasendrasoatsiry@gmail.com>
2025-06-30 21:51:36 +0200

    ecriture des donnees longues dans un fichier

--- get-data/main_get_data.R
+++ get-data/main_get_data.R
@@ -1,17 +1,26 @@
 here::i_am("get-data/main_get_data.R")
 
+source(here::here("config.R"))
 source(here::here("libraries.R"))
 source(here::here("get-data", "config.R"))
 
-household_data <- read_delim(here::here(path_hourly_data))
+if (!file.exists(here::here(path_hourly_data_long))) {
+  log_tictoc("Lecture du fichier {path_hourly_data}")
+  household_data <- read_delim(here::here(path_hourly_data))
 
-household_data_long <- household_data %>%
-  pivot_longer(
-    cols = all_of(cols_ener),
-    names_to = c("household_type", "id_household", "energy_type", "id_ener_source"),
-    values_to = "ener_kWh",
-    names_pattern = regex_col_names
-  ) %>%
-  mutate(
-    energy_type = str_replace(energy_type, "(.+)_$", "\\1") # on retire les _ a la fin des strings
-  )
+  household_data_long <- household_data %>%
+    pivot_longer(
+      cols = all_of(cols_ener),
+      names_to = c("household_type", "id_household", "energy_type", "id_ener_source"),
+      values_to = "ener_kWh",
+      names_pattern = regex_col_names
+    ) %>%
+    mutate(
+      energy_type = str_replace(energy_type, "(.+)_$", "\\1") # on retire les _ a la fin des strings
+    )
+  household_data_long %>%
+    write_csv(here::here(path_hourly_data_long))
+  log_tictoc("Donnees au pas horaire ecrites dans le fichier {path_hourly_data_long}")
+} else {
+  log_info("Fichier {path_hourly_data_long} deja existant. Rien a faire.")
+}
